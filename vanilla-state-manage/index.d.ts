0a2e73776816b30137f1d43ef5797d628fb1f52c
Modern-HD <hun7333@gmail.com>
2023-05-19 16:26:51 +0900

    지연 평가를 이용하여 vanilla js에서 상태 관리 도구 만들기

---
+++ vanilla-state-manage/index.d.ts
@@ -0,0 +1,15 @@
+declare namespace VanillaStateManager {
+
+    export interface Action {
+        type: string;
+    }
+
+    export interface Store<T extends Object, K extends Action> {
+        getState: () => T;
+        dispatch: (action: K) => void;
+    }
+
+    export type Reducer<T extends Object, K extends Action> = (state: T, action: K) => T;
+    export type CreateStore<T extends Object, K extends Action> = (initialState: T, reducer: Reducer<T, K>, subscribe: (state: T, action: K) => void) => Store<T, K>;
+    export type StoreGenerator<T extends Object, K extends Action> = (initialState: T, reducer: Reducer<T, K>, subscribe: (state: T, action: K) => void) => Generator<T, void, K>;
+}
---
+++ vanilla-state-manage/index.js
@@ -0,0 +1,59 @@
+/**
+ * @typedef {import('./index')} VanillaStateManager
+ */
+
+/** @type {VanillaStateManager.StoreGenerator<T, K>} */
+function* storeGenerator(initialState, reducer, subscribe) {
+    const state = initialState;
+    while (true) {
+        const action = yield;
+        console.log(action);
+        const newState = reducer(state, action);
+        subscribe && subscribe(newState, {...action});
+    }
+}
+
+/** @type {VanillaStateManager.CreateStore<T, K>} */
+function createStore(initialState, reducer, subscribe) {
+    const store = storeGenerator(initialState, reducer, subscribe);
+    store.next();
+    const storeObj = {
+        getState: () => {
+            return {...initialState};
+        },
+        dispatch: (action) => {
+            store.next(action);
+        }
+    }
+    return storeObj;
+}
+
+/** @type {VanillaStateManager.Reducer<T, K>} */
+function countReducer(state, action) {
+    switch(action.type) {
+        case "INCREASE":
+            state.value++;
+        return {...state};
+        case "DECREASE":
+            state.value--;
+        return {...state};
+    }
+}
+
+/** @type {VanillaStateManager.Store<{ value: number }, { type: "INCREASE" | "DECREASE" }>} */
+const countStore = createStore({value: 0}, countReducer, (state, action) => {
+    const valueZone = document.getElementById('counter-value');
+    if (action.type === "INCREASE" || action.type === "DECREASE") {
+        valueZone.innerText = state.value;
+    }
+})
+
+document.getElementById('increase').addEventListener('click', () => {
+    countStore.dispatch({ type: "INCREASE" });
+})
+document.getElementById('decrease').addEventListener('click', () => {
+    countStore.dispatch({ type: "DECREASE" });
+})
+document.getElementById('lookup-count-state').addEventListener('click', () => {
+    console.log(countStore.getState());
+})
